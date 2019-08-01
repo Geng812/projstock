75f33510807b0447876a904fa79f10da7da800ef
Geng812 <39304725+Geng812@users.noreply.github.com>
2019-08-01 17:16:09 +0800

    Update dividend__yield.py

--- dividend__yield.py
+++ dividend__yield.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+
 def main():
 
     Stock_ID = 
     Stock_Name = 
     Stock_Price = 
     
+    dividend = 
     amount = 
     date =
+    
+    Fun = Stock_Price / dividend
     
     
     
