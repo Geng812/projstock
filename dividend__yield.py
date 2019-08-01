873d501c35066251dd0d50e67c0d08c7db2e47d8
Geng812 <39304725+Geng812@users.noreply.github.com>
2019-08-01 16:50:59 +0800

    Create dividend__yield.py

---
+++ dividend__yield.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+
+    Stock_ID = 
+    Stock_Name = 
+    Stock_Price = 
+    
+    amount = 
+    date =
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
