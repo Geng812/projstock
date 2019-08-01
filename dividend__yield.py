54c8196fd4b2b3cc0f51b35ced4f68795cfa9202
Geng812 <39304725+Geng812@users.noreply.github.com>
2019-08-02 01:34:47 +0800

    Update dividend__yield.py

--- dividend__yield.py
+++ dividend__yield.py
@@ -12,7 +12,28 @@ def main():
     date =
     
     Fun = Stock_Price / dividend
+#stock = 1101
+
+#buy_price = 37.95
+
+#years = 1
+#amount = 1
+
+#dividends = [3.3]
+
+#print("Stock ID : ", stock,"\n")
+
+#i=0
+
+#while i  < years:
     
+#    dividend = int(amount * dividends[i] * 1000) 
+#    dividend_yield = ("%.2f" % ((dividends[i] / buy_price)*100))
+    
+#    print("The ", i," Year \nDividend yield : ", dividend_yield,"%")
+#    print("Dividend : ", dividend,"\n")
+    
+#    i+=1
     
     
 
