8796c4674a4f92971103a4c5b5f3d272db4375b1
Geng812 <39304725+Geng812@users.noreply.github.com>
2019-08-03 22:13:54 +0800

    Create DateAgoTest.py

---
+++ DateAgoTest.py
@@ -0,0 +1,24 @@
+import datetime
+import time
+
+def Get_nday_ageo(date, n)
+
+  nDayList = []
+
+  t = time.strptime(date, "%Y-%m-%d")
+  y, m, d = t[0:3]
+  
+  while n >= 0:
+  
+    D = str(datetime.datetime(y, m, d) - datetime.time
+    
+    sDate = D[0].split('-')
+    week = datetime.datetime(int(sDate[0], int(sDate[1]), int(sDate[2])).weekday()
+    
+    if week != 6 and week != 5 :
+    
+      nDayList.append(D[0])
+      
+    n -= 1
+      
+  return nDayList
